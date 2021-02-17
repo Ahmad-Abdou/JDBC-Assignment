d09ccaddc82e9b5d1392da0059912f2d55394295
Ahmad-Abdou <kyoub1996@gmail.com>
2021-02-17 11:43:03 +0100

    Find byId and find by code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.model.City;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,16 +14,12 @@ public class App
 {
     public static void main( String[] args ) {
 
+        City city = new City();
+        CityDaoJDBC cd = new CityDaoJDBC();
 
+        cd.findById(20);
+        System.out.println();
+        cd.findByCode("MOZ");
 
-    }
-    public static Connection mySqlConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
     }
 }
--- src/main/java/org/example/CityDaoJDBC.java
+++ src/main/java/org/example/CityDaoJDBC.java
@@ -4,6 +4,7 @@ import org.example.dao.CityDao;
 import org.example.model.City;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CityDaoJDBC implements CityDao {
@@ -11,13 +12,46 @@ public class CityDaoJDBC implements CityDao {
 
     @Override
     public City findById(int id) {
+        City city = new City();
+        Connection connection = mySqlConnection();
+        String query = "select id,name from city where id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " - " + resultSet.getString("name"));
+            } else {
+                System.out.println(" No data ");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        return null;
+        return city;
     }
 
     @Override
     public List<City> findByCode(String code) {
-        return null;
+        List<City> list = new ArrayList<>();
+        Connection connection = mySqlConnection();
+       String query = "select id,name,countryCode from city where countryCode = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,code);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String countryCode = resultSet.getString("countryCode");
+                list.add(new City(id,name,countryCode));
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
     }
 
     @Override
